a544fd4ce4780c67862bf882ac387746b5349eaf
Chloride Cull <chloride@devurandom.net>
2016-01-30 17:05:51 +0100

    add license and lic headers

--- config.py
+++ config.py
@@ -1,3 +1,16 @@
+# 4chThreadArchiver
+# Copyright (C) 2016, Sebastian "Chloride Cull" Johansson
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
 ARCHIVE_SITE="https://desustorage.org"
 UA="4chThreadArchiver/1.0 (Python {vinfo[0]}.{vinfo[1]}.{vinfo[2]}, using urllib)"
 #UA="Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.71 Safari/537.36"
