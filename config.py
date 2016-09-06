24da68c145750ae230a7a855a1daf3cd97a813d8
Chloride Cull <chloride@devurandom.net>
2016-09-06 13:47:42 +0200

    Change desustorage URL to desuarchive

--- config.py
+++ config.py
@@ -11,6 +11,6 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
 
-ARCHIVE_SITE="https://desustorage.org"
+ARCHIVE_SITE="https://desuarchive.org"
 UA="4chThreadArchiver/1.0 (Python {vinfo[0]}.{vinfo[1]}.{vinfo[2]}, using urllib)"
 #UA="Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.71 Safari/537.36"
